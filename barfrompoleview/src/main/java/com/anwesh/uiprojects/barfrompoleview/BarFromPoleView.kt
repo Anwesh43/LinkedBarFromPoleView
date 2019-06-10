b171e5b8a4b54519d232fa1cba7fba000ca8ca83
Anwesh43 <anweshthecool0@gmail.com>
2019-06-11 03:32:51 +0530

    adding libraries and modules

--- barfrompoleview/src/main/java/com/anwesh/uiprojects/barfrompoleview/BarFromPoleView.kt
+++ barfrompoleview/src/main/java/com/anwesh/uiprojects/barfrompoleview/BarFromPoleView.kt
@@ -3,3 +3,21 @@ package com.anwesh.uiprojects.barfrompoleview
 /**
  * Created by anweshmishra on 11/06/19.
  */
+
+import android.app.Activity
+import android.graphics.Color
+import android.graphics.RectF
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.content.Context
+import android.view.View
+import android.view.MotionEvent
+
+val nodes : Int = 5
+val lines : Int = 4
+val scGap : Float = 0.05f
+val scDiv : Double = 0.51
+val strokeFactor : Int = 90
+val sizeFactor : Float = 2.9f
+val foreColor : Int = Color.parseColor("#4CAF50")
+val backColor : Int = Color.parseColor("#BDBDBD")
