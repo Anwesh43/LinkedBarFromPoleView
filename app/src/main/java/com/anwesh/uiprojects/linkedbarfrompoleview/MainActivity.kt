be3364704920f7067718fd96046ec70d215b8c76
Anwesh43 <anweshthecool0@gmail.com>
2019-06-11 04:01:26 +0530

    adding view in activity

--- app/src/main/java/com/anwesh/uiprojects/linkedbarfrompoleview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedbarfrompoleview/MainActivity.kt
@@ -2,11 +2,12 @@ package com.anwesh.uiprojects.linkedbarfrompoleview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.anwesh.uiprojects.barfrompoleview.BarFromPoleView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        BarFromPoleView.create(this)
     }
 }
